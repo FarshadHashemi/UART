7eb1d9a3aecf4b61436854500ab96810fd0c4917
FarshadHashemi <121182056+FarshadHashemi@users.noreply.github.com>
2023-01-17 20:52:50 +0330

    Update UART_Transmitter.vhd

--- UART_Transmitter.vhd
+++ UART_Transmitter.vhd
@@ -24,17 +24,17 @@ End UART_Transmitter ;
 
 Architecture Behavioral Of UART_Transmitter Is
 
-   Signal Input_Register             : STD_Logic_Vector(Data_Bits-1 Downto 0) := (Others=>'0') ;
-   Signal Available_Input_Register   : STD_Logic                              := '0' ;
-   Signal Output_Register            : STD_Logic                              := '1' ;
-   Signal Busy_Register              : STD_Logic                              := '0' ;
+   Signal Input_Register           : STD_Logic_Vector(Data_Bits-1 Downto 0)                       := (Others=>'0') ;
+   Signal Available_Input_Register : STD_Logic                                                    := '0' ;
+   Signal Output_Register          : STD_Logic                                                    := '1' ;
+   Signal Busy_Register            : STD_Logic                                                    := '0' ;
 
-   Signal Data_Line                  : STD_Logic_Vector(Data_Bits+Parity_Enable+Stop_Bits Downto 0) := (Others=>'0') ;
+   Signal Data_Line                : STD_Logic_Vector(Data_Bits+Parity_Enable+Stop_Bits Downto 0) := (Others=>'0') ;
 
-   Signal Parity                     : STD_Logic_Vector(Data_Bits-2 Downto 0) := (Others=>'0') ;
+   Signal Parity                   : STD_Logic_Vector(Data_Bits-2 Downto 0)                       := (Others=>'0') ;
 
-   Signal Time_Counter               : Integer Range 0 To (((Clock_Frequency*(10**6))/Baud_Rate)-1) := 0 ;
-   Signal Bit_Counter                : Integer Range 0 To (Data_Bits+Parity_Enable+Stop_Bits) := 0 ;
+   Signal Time_Counter             : Integer Range 0 To (((Clock_Frequency*(10**6))/Baud_Rate)-1) := 0 ;
+   Signal Bit_Counter              : Integer Range 0 To (Data_Bits+Parity_Enable+Stop_Bits)       := 0 ;
 
 Begin
 
@@ -55,18 +55,18 @@ Begin
          If (Busy_Register='0') And (Available_Input_Register='1') Then
 
             Data_Line(Data_Bits Downto 1) <= Input_Register ;
-            Busy_Register <= '1' ;
+            Busy_Register                 <= '1' ;
       -- %%%%%%%%%%%%%%%%%
 
          Elsif Busy_Register='1' Then
 
          -- Time Control
-            Time_Counter <= Time_Counter + 1 ;
+            Time_Counter        <= Time_Counter + 1 ;
             If Time_Counter=(((Clock_Frequency*(10**6))/Baud_Rate)-1) Then
-               Time_Counter <= 0 ;
-               Bit_Counter <= Bit_Counter + 1 ;
+               Time_Counter     <= 0 ;
+               Bit_Counter      <= Bit_Counter + 1 ;
                If Bit_Counter=(Data_Bits+Parity_Enable+Stop_Bits) Then
-                  Bit_Counter <= 0 ;
+                  Bit_Counter   <= 0 ;
                   Busy_Register <= '0' ;
                End If ;
                End If ;
@@ -74,7 +74,7 @@ Begin
 
          -- Preity Bit Calculation
             If Parity_Enable=1 Then
-               Parity(0) <= Data_Line(1) Xor Data_Line(2) ;
+               Parity(0)      <= Data_Line(1) Xor Data_Line(2) ;
                For i In 3 To Data_Bits Loop
                   Parity(i-2) <= Data_Line(i) Xor Parity(i-3) ;
                End Loop ;
